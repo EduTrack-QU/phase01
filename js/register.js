89c1999b1dda1826118ddd5b75a98f8bdf7c8123
ansari <elansari1211@gmail.com>
2025-03-21 05:10:55 +0300

    added fetching courses in registration

--- js/register.js
+++ js/register.js
@@ -1,3 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.querySelector('form');
     
-})
+    loginForm.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        
+        const courseCode = document.querySelector('input[name="course_code"]').value;
+        
+        
+            const response = await fetch('/json/courses.json');
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const courses = await response.json();
+            
+            const course = courses.find(c => 
+                c.course_code.toLowerCase() === courseCode.toLowerCase() && c.open === true
+            );
+
+            console.log(course)
+        
+            
+    
+    });
+}); 
