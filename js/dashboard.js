9ea83a5e7d51ddc1396ad3de265f7297bf7f6799
ansari <elansari1211@gmail.com>
2025-03-24 01:58:13 +0300

    removed useless script files

--- js/dashboard.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const userName = localStorage.getItem('userName');
-    if (userName) {
-        document.querySelector('h2').textContent = `Welcome ${userName} To the Portal`;
-    }
-}); 
