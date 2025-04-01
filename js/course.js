6d14a79e54cf84035ba5995bf9b2259faf3510f3
ansari <elansari1211@gmail.com>
2025-04-01 13:49:06 +0300

    added id attribute to courses

--- js/course.js
+++ js/course.js
@@ -1,13 +1,23 @@
+import { nanoid } from 'nanoid';
 class Course {
-
     constructor(code,title, creditHour,description, instructorId, available) {
-        this.code=code;
+        this.code=code?? nanoid(6);
         this.title = title;
         this.creditHour = creditHour; // number of credit hours
         this.description = description;
         this.instructorId = instructorId; // instructor ID
         this.available = available ?? false;
         this.prerequisites = []; // array of course IDs
+    }
+
+    fromJSON(json) {
+        this.code = json.code;
+        this.title = json.title;
+        this.creditHour = json.creditHour;
+        this.description = json.description;
+        this.instructorId = json.instructorId;
+        this.available = json.available;
+        this.prerequisites = json.prerequisites || [];
     }
     
 }
