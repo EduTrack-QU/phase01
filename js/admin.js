0f15dda15a8b15610564162c76ec15d2024a2205
ansari <elansari1211@gmail.com>
2025-04-09 14:15:20 +0300

    removed empty methods in admin class

--- js/admin.js
+++ js/admin.js
@@ -5,22 +5,6 @@ export class Admin extends User {
       super(name, username, 'admin');
     }
   
-    createUser(userObject) {
-      // push to users.json
-    }
-  
-    deleteUser(userId) {
-      // remove from users.json
-    }
-  
-    createCourse(courseObject) {
-      // push to courses.json
-    }
-  
-    deleteCourse(courseId) {
-      // remove from courses.json
-    }
-  
     toJSON() {
       return super.toJSON();
     }
